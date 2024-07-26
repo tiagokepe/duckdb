c920b19dd8606889550a206bf9ada3314de69a13
Tiago Kepe <tiagokepe@gmail.com>
2024-07-26 16:35:41 +0100

    Added bound_aggregate_expression.hpp

--- src/planner/binder/query_node/plan_select_node.cpp
+++ src/planner/binder/query_node/plan_select_node.cpp
@@ -1,4 +1,5 @@
 #include "duckdb/planner/binder.hpp"
+#include "duckdb/planner/expression/bound_aggregate_expression.hpp"
 #include "duckdb/planner/expression/bound_columnref_expression.hpp"
 #include "duckdb/planner/expression/bound_function_expression.hpp"
 #include "duckdb/planner/expression/bound_reference_expression.hpp"
